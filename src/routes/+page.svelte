9dfacfbcc75a14e1681bf903465df8a7877b842c
Simon Zinsli <simon@zinsli.co>
2024-11-06 21:49:19 +1100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-full">
+	<img class="m-auto block w-full max-w-lg" src="/logo-large.png" alt="Success In Maths" />
+	<div class="m-auto text-center text-lg sm:text-base">
+		Inquiries: <a class="hover:underline" href="mailto:admin@successinmaths.com.au">admin@successinmaths.com.au</a>
+        <a class="hover:grayscale" href="https://www.facebook.com/profile.php?id=61568327004662"><img class="inline w-8 mx-4" src="/facebook.svg" alt="Success In Maths Facebook"></a>
+	</div>
+</div>
